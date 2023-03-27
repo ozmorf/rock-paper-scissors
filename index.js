8c34a557d041b3215fab846e27b72d067c72e251
Anthony Apodaca <aapodaca500@gmail.com>
2023-03-27 15:35:31 -0600

    add sections to CSS, pseudocode to JS file.

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 let elements = ["Rock", "Paper", "Scissors"]
 let computerNumber = Math.floor((Math.random() * 3))
 let computerChoice = elements[computerNumber]
+
+// get user's input:
+// 1) check input to see if it's valid
+// 2) if valid, store in variable
+
+// create logic to check who wins the round
+
+// display text to show who wins
