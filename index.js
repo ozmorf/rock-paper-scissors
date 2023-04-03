ae28fd5e2562774d48726f529fc9a5abd35560e8
Anthony Apodaca <aapodaca500@gmail.com>
2023-04-03 13:45:52 -0600

    Create working version of rps which runs in console

--- index.js
+++ index.js
@@ -1,41 +1,65 @@
 
 let elements = ["rock", "paper", "scissors"]
-let computerNumber = Math.floor((Math.random() * 3))
-let computerChoice = elements[computerNumber]
+let userPoints = 0
+let computerPoints = 0 
+let validChoice = false
+let computerElement = ""
+let userElement = ""
 
 // get user input, make sure it's rock, paper, or scissors
 
 function getUserElement() {
-    let userChoice = prompt()
-    checkUserElement(userChoice)
-    return userChoice
+
+    while (!validChoice) {
+        //prompt for new input
+        let userInput = prompt("type in \'rock\', \' paper\', or \' scissors\'.")
+        //check if it's valid
+        checkUserElement(userInput)
+        if (validChoice) {
+            validChoice = false
+            return userElement = userInput
+        }
+    }
+    //once we exit the while block, we have a valid input
+    
 }
 
 function checkUserElement(item) {
 
     if (elements.includes(item)) {
-        console.log(`Accepted. You entered ${item}.`)
-        return item
+        //console.log(`Accepted. You entered ${item}.`)
+        validChoice = true
+        //return item
     } else {
         console.log("the input you enetered wasn't valid. Try again.")
-        getUserElement()
+        //getUserElement()
     }
 }
 
-let userElement = getUserElement()
-console.log(`you chose ${userElement} and the computer chose ${computerChoice}`)
+// generate computer element
+
+function generateComputerElement() {
+    let computerNumber = Math.floor((Math.random() * 3))
+    let computerChoice = elements[computerNumber]
+    return computerElement = computerChoice
+    //console.log(`computer element: ${computerChoice}`)
+    //return computerChoice
+}
 
 // determine winner from player and computer selection
 
 function determineWinner(playerSelection, computerSelection) {
-    // create logic for all win conditions for the player
 
     function displayWinMessage() {
+        ++userPoints
         return(`You chose ${playerSelection} and the computer chose ${computerSelection}. YOU WIIIIIN!!!!!.`)
     }
+
     // if it's a tie...
     if (playerSelection === computerSelection) {
-        console.log(`Impossible! You both chose ${playerSelection}! I don't even know what to do!`)
+        console.log(`You both chose ${playerSelection}. You each get a point`)
+        ++userPoints
+        ++computerPoints
     }
 
     // all the conditions where the user wins
@@ -51,7 +75,25 @@ function determineWinner(playerSelection, computerSelection) {
     // If the player doesn't win or tie, the player loses.
     } else {
         console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}. That means you're a loser.`)
+        ++computerPoints
     }
 }
 
-determineWinner(userElement, computerChoice)
+
+
+function game() {
+    // get user and computer element
+    let computerElement = generateComputerElement()
+    let userElement = getUserElement()
+    //console.log(`you chose ${userElement} and the computer chose ${computerChoice}`)
+
+    // determine winner
+    determineWinner(userElement, computerElement)
+    console.log(`You have ${userPoints} points and the computer has ${computerPoints} points.`)
+}
+
+setTimeout(() => {
+    for (i=0; i<4; i++) {
+        game()
+    }
+}, 10)
