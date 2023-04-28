d8658a604ca2d2ac705cec4bd377e8d6346a56c4
Anthony Apodaca <aapodaca500@gmail.com>
2023-04-28 15:06:49 -0600

    Create working version of game with minimal bugs.

--- exp.js
+++ exp.js
@@ -15,13 +15,23 @@ function displayComputerElement(element) {
     computerElementText.textContent = element
 }
 
+function displayPoints(userPoints, computerPoints) {
+    // querySelect right DOM element to display the points
+    // update the points
+    let userScore = document.querySelector('.user-score')
+    userScore.textContent = userPoints
+
+    let computerScore = document.querySelector('.computer-score')
+    computerScore.textContent = computerPoints
+}
+
 function determineWinner(userE, computerE) {
     if (userE === computerE) {
         console.log("tie")
         return "tie"
-    } else if (userE === 'rock' && computerE === 'scissors' || userE === 'paper' && computerE === 'rock' || userE === 'scissors' && computerE === 'paper' ) {
+    } else if (userE === 'rock' && computerE === 'scissors' || userE === 'paper' && computerE === 'rock' || userE === 'scissors' && computerE === 'paper') {
         console.log("true")
-        return true 
+        return true
     } else {
         console.log("false")
         return false
@@ -31,32 +41,43 @@ function determineWinner(userE, computerE) {
 
 function game() {
     let rounds = 0
-    userPoints = 0
-    computerPoints = 0
+    let userPoints = 0
+    let computerPoints = 0
+    let choiceCards = [...document.querySelectorAll('.card')]
 
-    for (i = 0; i < 5; i++) {
-        let computerElement = generateComputerElement()
+    choiceCards.forEach(card => card.addEventListener('click', (_event) => {
 
-        let choiceCards = [...document.querySelectorAll('.card')]
-        choiceCards.forEach(card => card.addEventListener('click', (_event) => {
+        if (rounds < 5) {
+            let computerElement = generateComputerElement()
             displayUserElement(card.id)
             displayComputerElement(computerElement)
             let outcome = determineWinner(card.id, computerElement)
+
             if (outcome === 'tie') {
                 console.log("it's a tie")
+                userPoints++
+                computerPoints++
             } else if (outcome) {
                 console.log("you win!")
+                userPoints++
             } else if (!outcome) {
                 console.log("you lose")
+                computerPoints++
             }
-            rounds++
-        }))
-    }
 
-    // get user element
-    // display user and computer element
-    // score appropriately
-    // play 5 rounds
+            displayPoints(userPoints, computerPoints)
+            rounds++
+        } else {
+            console.log('finshed')
+            return userPoints, computerPoints
+        }
+    }))
 }
 
-game()
+let score = game()
+//console.log(`Game finished. You got ${userPoints} points, and the computer got ${computerPoints} points`)
+console.log('score: ' + score)
+
+// Win screen: blur entire page, and display a win message, with a prompt to play again by clicking a button
+// Add mouse-over color change on clickable buttons
+// Display a message after every round (use seperate file)
